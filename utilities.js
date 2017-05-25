bba2b1c34f1c61e0d22115ce1994b7d55cb6764a
Elaine Howard <erhoward-github@users.noreply.github.com>
2017-05-25 01:43:00 -0400

    Fixed "global" variable

--- utilities.js
+++ utilities.js
@@ -1,6 +1,16 @@
-if(typeof global !== "undefined")
+/*global jQuery, $ */
+
+if(typeof global === "undefined")
 {
-    var global = window;
+    var global = null;
+    if(typeof(window) !== "undefined")
+    {
+        global = window;
+    }
+    else
+    {
+        global = this;
+    }
 }
 
 if(typeof UTIL === "undefined")
@@ -177,12 +187,8 @@ UTIL.getSubCookie = function(cookieName, subCookieName)
     return "";
 }; // end UTIL.getSubCookie = function(cookieName, subCookieName)
 
-UTIL.trace = function (message)
+UTIL.log = function (message)
 {
-    //if(UTIL.getValueFromURL("debug") !== "1")
-    //{
-    //    return;
-    //}
     if(typeof console !== "undefined")
     {
         if(typeof console.log !== "undefined")
@@ -200,7 +206,16 @@ UTIL.trace = function (message)
             }
         }
     }
-}; // end UTIL.trace = function (message)
+}; // end UTIL.log = function (message)
+
+UTIL.debug = function (message)
+{
+    if(UTIL.getValueFromURL("debug") !== "1")
+    {
+        return;
+    }
+    UTIL.log(message);
+}; // end UTIL.debug = function (message)
 
 UTIL.getGuid = function()
 {
@@ -218,3 +233,147 @@ UTIL.getGuid = function()
     guid = "guid-" + makeSegment(8) + "-" + makeSegment(4) + "-" + makeSegment(4) + "-" + makeSegment(4) + "-" + makeSegment(12);
     return guid;
 }; // end UTIL.getGuid = function()
+
+UTIL.compareDates = function(dOne, dTwo)
+{
+    if(+dOne < +dTwo)
+    {
+        return -1;
+    }
+    else if(+dOne === +dTwo)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}; // end UTIL.compareDates = function(dOne, dTwo)
+
+UTIL.isValidDate = function(strText)
+{
+    var blnValid;
+    var nDate;
+
+    if(strText === "" || strText === null)
+    {
+        return true;
+    }
+    nDate = Date.parse(strText);
+    blnValid = true;
+    if(isNaN(nDate))
+    {
+        blnValid = false;    
+    }
+
+    //alert(strText + " is " + (blnValid ? "valid" : "invalid"));
+    return blnValid;
+}; // end UTIL.isValidDate = function(strText)
+
+UTIL.isSafeDataEntry = function(strText)
+{
+    var pattern = /[^a-zA-Z'".,;:0-9\-\?\s]+/;
+    var isOK = !pattern.test(strText);
+    return isOK;
+}; // end UTIL.isSafeDataEntry = function(strText)
+
+UTIL.isValidEmailAddress = function(strText)
+{
+    var blnValid;
+    var re;
+
+    if(strText === "" || strText === null)
+    {
+      return true;
+    }
+    // erhoward@prodigy.net
+    re = /^[^@]+@([a-z0-9\-]+\.)+[a-z]{2,4}$/i;
+    blnValid = re.test(strText);
+    return blnValid;
+}; // UTIL.isValidEmailAddress = function(strText)
+
+UTIL.isValidPhoneNumber = function(strText)
+{
+    var blnValid;
+    var re;
+
+    if(strText === "" || strText === null)
+    {
+        return true;
+    }
+    // 609-882-2489 or (609) 882-2489
+    re = /^((\d{3}-\d{3}-\d{4})|(\(\d{3}\)\s\d{3}-\d{4}))$/i;
+
+    blnValid = re.test(strText);
+    return blnValid;
+}; // end UTIL.isValidPhoneNumber = function(strText)
+
+UTIL.isValidSocialSecurityNumber = function(strText)
+{
+    var blnValid;
+    var re;
+
+    if(strText === "" || strText === null)
+    {
+        return true;
+    }
+    re = /^\d{3}-\d{2}-\d{4}$/i;  
+    blnValid = re.test(strText);
+    //alert(strText + " is " + (blnValid ? "valid" : "invalid"));
+    return blnValid;
+}; // end UTIL.isValidSocialSecurityNumber = function(strText)
+
+UTIL.isValidZipCode = function(strText)
+{
+    var blnValid;
+    var re;
+
+    if(strText === "" || strText === null)
+    {
+        return true;
+    }
+    re = /^\d{5}-\d{4}$/i;  
+    blnValid = re.test(strText);
+    //alert(strText + " is " + (blnValid ? "valid" : "invalid"));
+    return blnValid;
+}; // end UTIL.isValidZipCode = function(strText)
+
+;(function($){
+    $.fn.dateHighlighter = function(optionsIn){
+        var options = $.extend({}, $.fn.dateHighlighter.defaults, optionsIn);
+        return this.each(function(){
+            var END_OF_WEEK_DAY = 6;
+            var BEGIN_OF_WEEK_DAY = 0;
+            var userDateIn = $(this).attr("data-date-highlighter-date") || "";
+            if(isNaN(Date.parse(userDateIn)))
+            {
+                 return;             
+            }
+            var userDate = new Date(Date.parse(userDateIn));
+            var userDay = userDate.getDay();
+            
+            var currentDate = new Date();
+            var currentDay = currentDate.getDay();
+            
+            var endWeekOffset = END_OF_WEEK_DAY - userDay;
+            var beginWeekOffset = userDay - BEGIN_OF_WEEK_DAY;
+            
+            var endWeekDate = new Date(+userDate);
+            var beginWeekDate = new Date(+userDate);
+            
+            beginWeekDate.setDate(beginWeekDate.getDate() - beginWeekOffset);
+            endWeekDate.setDate(endWeekDate.getDate() + endWeekOffset);
+            
+            if(+currentDate <= +beginWeekDate)
+            {
+                $(this).addClass("active");
+                if($(this).hasClass("panel"))
+                {    
+                    $(this).addClass("panel-info");
+                }
+            }
+        });
+    };
+    $.fn.dateHighlighter.defaults = {
+    };
+})(jQuery);
